9bd5ce5cce51ca553d8e3e8bd8f599ff71f04981
Natalya.Kadysheva <Native1331>
2022-06-10 00:50:00 +0300

    Add Test

---
+++ src/test/java/tests/WebShopTest.java
@@ -0,0 +1,133 @@
+package tests;
+import com.codeborne.selenide.WebDriverRunner;
+import io.qameta.allure.restassured.AllureRestAssured;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.Cookie;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Configuration.baseUrl;
+import static com.codeborne.selenide.Selenide.*;
+import static helpers.CustomApiListener.withCustomTemplates;
+import static io.qameta.allure.Allure.step;
+import static io.restassured.RestAssured.given;
+
+
+public class WebShopTest extends TestBase{
+  String login;
+  String password;
+  String authCookieName;
+    @Test
+    @Tag("demowebshop")
+    @DisplayName("Successful authorization to some demowebshop (UI)")
+    void loginTest() {
+        step("Open login page", () ->
+                open(""));
+
+        step("Fill login form", () -> {
+            $("#Email").setValue(login);
+            $("#Password").setValue(password)
+                    .pressEnter();
+        });
+
+        step("Verify successful authorization", () ->
+                $(".account").shouldHave(text(login)));
+    }
+
+    @Test
+    @Tag("demowebshop")
+    @DisplayName("Successful authorization to some demowebshop (API + UI)")
+    void loginWithApiTest() {
+        step("Get cookie by api and set it to browser", () -> {
+            String authCookieValue = given()
+                    .contentType("application/x-www-form-urlencoded")
+                    .formParam("Email", login)
+                    .formParam("Password", password)
+//                .body("Email=" + login + "&Password=" + password + "&RememberMe=false")
+//                .body(format("Email=%s&Password=%s&RememberMe=false", login, password))
+                    .log().all()
+                    .when()
+                    .post("/login")
+                    .then()
+                    .log().all()
+                    .statusCode(302)
+                    .extract().cookie(authCookieName);
+
+            step("Open minimal content, because cookie can be set when site is opened", () ->
+                    open("/Themes/DefaultClean/Content/images/logo.png"));
+            step("Set cookie to to browser", () -> {
+                Cookie authCookie = new Cookie(authCookieName, authCookieValue);
+                WebDriverRunner.getWebDriver().manage().addCookie(authCookie);
+            });
+        });
+
+        step("Open main page", () ->
+                open(""));
+        step("Verify successful authorization", () ->
+                $(".account").shouldHave(text(login)));
+    }
+
+    @Test
+    @Tag("demowebshop")
+    @DisplayName("Successful authorization to some demowebshop (API + UI)")
+    void loginWithApiAndAllureListenerTest() {
+        step("Get cookie by api and set it to browser", () -> {
+            String authCookieValue = given()
+                    .filter(new AllureRestAssured())
+                    .contentType("application/x-www-form-urlencoded")
+                    .formParam("Email", login)
+                    .formParam("Password", password)
+                    .log().all()
+                    .when()
+                    .post("/login")
+                    .then()
+                    .log().all()
+                    .statusCode(302)
+                    .extract().cookie(authCookieName);
+
+            step("Open minimal content, because cookie can be set when site is opened", () ->
+                    open("/Themes/DefaultClean/Content/images/logo.png"));
+            step("Set cookie to to browser", () -> {
+                Cookie authCookie = new Cookie(authCookieName, authCookieValue);
+                WebDriverRunner.getWebDriver().manage().addCookie(authCookie);
+            });
+        });
+
+        step("Open main page", () ->
+                open(""));
+        step("Verify successful authorization", () ->
+                $(".account").shouldHave(text(login)));
+    }
+
+    @Test
+    @Tag("demowebshop")
+    @DisplayName("Successful authorization to some demowebshop (API + UI)")
+    void loginWithApiAndCustomListenerTest() {
+        step("Get cookie by api and set it to browser", () -> {
+            String authCookieValue = given()
+                    .filter(withCustomTemplates())
+                    .contentType("application/x-www-form-urlencoded")
+                    .formParam("Email", login)
+                    .formParam("Password", password)
+                    .log().all()
+                    .when()
+                    .post("/login")
+                    .then()
+                    .log().all()
+                    .statusCode(302)
+                    .extract().cookie(authCookieName);
+
+            step("Open minimal content, because cookie can be set when site is opened", () ->
+                    open("/Themes/DefaultClean/Content/images/logo.png"));
+            step("Set cookie to to browser", () -> {
+                Cookie authCookie = new Cookie(authCookieName, authCookieValue);
+                WebDriverRunner.getWebDriver().manage().addCookie(authCookie);
+            });
+        });
+
+        step("Open main page", () ->
+                open(""));
+        step("Verify successful authorization", () ->
+                $(".account").shouldHave(text(login)));
+    }
+}
+
